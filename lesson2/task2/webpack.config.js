0b5d7538c28af146e1fea29a82fa269e4f666a0e
Ozirska <m.ozirska2000@gmail.com>
2023-03-13 23:44:33 +0100

    clock with JSX

--- lesson2/task2/webpack.config.js
+++ lesson2/task2/webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
