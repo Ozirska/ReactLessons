9ef6857299b780875f2bc33c54d8547a7d15cead
Ozirska <m.ozirska2000@gmail.com>
2023-03-14 10:15:04 +0100

    c

--- lesson2/todolist/src/index.js
+++ lesson2/todolist/src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.querySelector("#root");
 
 const element = (
   <>
-    <h1 className="title">Todo List</h1>;
+    <h1 className="title">Todo List</h1>
     <main className="todo-list">
       <div className="actions">
         <input className="task-input" type="text" />
