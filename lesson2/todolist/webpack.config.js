2c39cd09a79417064586b85dcde0cbf00ff29089
Ozirska <m.ozirska2000@gmail.com>
2023-03-14 10:10:06 +0100

    todo list +JSX

---
+++ lesson2/todolist/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
