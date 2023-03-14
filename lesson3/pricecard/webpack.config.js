44ff8a0b76dfea1ffdcb8fd65665036c14130411
Ozirska <m.ozirska2000@gmail.com>
2023-03-14 13:24:56 +0100

    Functional components

---
+++ lesson3/pricecard/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
