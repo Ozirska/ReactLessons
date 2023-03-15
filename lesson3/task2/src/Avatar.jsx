e077f290dc13ee2e93f435f51eb86300b77fc67c
Ozirska <m.ozirska2000@gmail.com>
2023-03-15 09:17:58 +0100

    Split Comment component

--- lesson3/task2/src/Avatar.jsx
+++ lesson3/task2/src/Avatar.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const UserAvatar = (props) => {
-  return (
-    <img className="avatar" src={props.user.avatarUrl} alt={props.user.name} />
-  );
+  return <img className="avatar" src={props.avatarUrl} alt={props.name} />;
 };
 
 export default UserAvatar;
--- lesson3/task2/src/UserInfo.jsx
+++ lesson3/task2/src/UserInfo.jsx
@@ -4,7 +4,7 @@ import UserAvatar from "./Avatar.jsx";
 const UserInfo = (props) => {
   return (
     <div className="user-info">
-      <UserAvatar user={props.user} />
+      <UserAvatar avatarUrl={props.user.avatarUrl} name={props.user.name} />
       <div className="user-info__name">{props.user.name}</div>
     </div>
   );
