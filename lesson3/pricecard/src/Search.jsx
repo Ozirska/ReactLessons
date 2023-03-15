8febab3e48c6aac04f5e15a001f131feff2c1c05
Ozirska <m.ozirska2000@gmail.com>
2023-03-15 09:07:45 +0100

    Split Comment component

--- lesson3/pricecard/src/Search.jsx
+++ lesson3/pricecard/src/Search.jsx
@@ -12,3 +12,17 @@ export default function Greeting(props) {
   );
   return element;
 }
+
+// class SearchField extends Component {
+//   render() {
+//     return (
+//       <div className="search">
+//         <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>{" "}
+//         <div className="search__field">
+//           <input type="text" className="search__input" />
+//           <button className="search__button">Search</button>{" "}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
