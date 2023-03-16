cc55c900d87bb5fabf4d5a552e254ac76f90c3d6
Ozirska <m.ozirska2000@gmail.com>
2023-03-16 07:33:09 +0100

    c

--- lesson3/task4/src/index.jsx
+++ lesson3/task4/src/index.jsx
@@ -4,19 +4,11 @@ import Profile from "./Profile.jsx";
 
 const rootElement = document.querySelector("#root");
 
-// const userData = {
-//   firstName: "James",
-//   lastName: "Bond",
-//   birthDate: "1991-01-17T11:11:11.819Z",
-//   birthPlace: "London",
-// };
+const userData = {
+  firstName: "James",
+  lastName: "Bond",
+  birthDate: "1991-01-17T11:11:11.819Z",
+  birthPlace: "London",
+};
 
-ReactDOM.render(
-  <Profile
-    firstName="James"
-    lastName="Bond"
-    birthDate="1991-01-17T11:11:11.819Z"
-    birthPlace="London"
-  />,
-  rootElement
-);
+ReactDOM.render(<Profile userData={userData} />, rootElement);
