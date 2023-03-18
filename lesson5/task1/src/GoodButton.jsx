d5de12ee88d311f4c99c33e33040a632e1c13104
Ozirska <m.ozirska2000@gmail.com>
2023-03-18 18:10:49 +0100

    + target

--- lesson5/task1/src/GoodButton.jsx
+++ lesson5/task1/src/GoodButton.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
-const GoodButton = () => (
-  <button className="fancy-button" onClick={() => alert("Good job!")}>
-    Click me!
-  </button>
-);
+// const GoodButton = () => (
+//   <button className="fancy-button" onClick={() => alert("Good job!")}>
+//     Click me!
+//   </button>
+// );
+
+class GoodButton extends React.Component {
+  handleClick(e) {
+    console.log(e.target.textContent);
+    alert("Good job!");
+  }
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
 
 export default GoodButton;
