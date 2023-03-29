0803b19f42de321ea76b8a8253ec3bb61f9b0f38
Ozirska <m.ozirska2000@gmail.com>
2023-03-29 22:58:25 +0200

    c

--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -8,20 +8,4 @@ const Status = (props) => {
   );
 };
 
-// class Status extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = { isOnline: props.status };
-//   }
-
-//   render() {
-//     return (
-//       <div className="status">
-//         {this.state.isOnline ? <Online /> : <Offline />}
-//       </div>
-//     );
-//   }
-// }
-
 export default Status;
